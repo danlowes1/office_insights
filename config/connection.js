a31731c02a3efc203e92dd3f6b85a08c3048ba1d
danlowes1 <dan141@gmail.com>
2025-09-03 07:20:42 +0100

    connection.js for SSL / postgres

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,52 @@
 // config/connection.js
-require("dotenv").config();
-
 const Sequelize = require("sequelize");
 
-if (process.env.DB_PASSWORD === "ChangeMe!") {
-  console.error("Please update the .env file with your database password.");
-  process.exit(1);
-}
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Render requires SSL
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_DATABASE,
       process.env.DB_USERNAME,
       process.env.DB_PASSWORD,
       {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT || "postgres",
+        port: process.env.DB_PORT || 5432,
         logging: false,
       }
     );
 
 module.exports = sequelize;
+
+
+
+// const Sequelize = require("sequelize");
+
+// if (process.env.DB_PASSWORD === "ChangeMe!") {
+//   console.error("Please update the .env file with your database password.");
+//   process.exit(1);
+// }
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(
+//       process.env.DB_DATABASE,
+//       process.env.DB_USERNAME,
+//       process.env.DB_PASSWORD,
+//       {
+//         host: process.env.DB_HOST,
+//         dialect: process.env.DB_DIALECT,
+//         port: process.env.DB_PORT,
+//         logging: false,
+//       }
+//     );
+
+// module.exports = sequelize;
